a515ec1f8133d1db923806d73879ebdba3309197
KAldhalimi <karrarmajed@gmail.com>
2019-01-27 18:47:11 -0500

    initial commit

---
+++ circle.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab10
+{
+    class circle
+    {
+        //data members
+        #region DataMembers
+        private double radius;
+        #endregion
+
+
+        //Properties
+        #region Properties
+        public double Radius
+        {
+            set
+            {
+                radius = value;
+            }
+            get
+            {
+                return radius;
+            }
+        }
+        #endregion
+
+        #region Methods
+        public double CalculateCircumference()
+        {
+            
+            double circum = (2 * (Math.PI * Radius));
+            return circum;
+        }
+        public double CalculateArea()
+        {
+            double area = (Math.PI * (Radius * Radius));
+            return area;
+        }
+
+        public string CalculateFormattedCircumference()
+        {
+            string formattedString = $"{CalculateCircumference(), 0:N2}";
+            return formattedString;
+        }
+
+        public string CalculateFormattedArea()
+        {
+            string formattedArea = $"{CalculateArea(),0:N2}";
+
+                return formattedArea;
+        }
+
+        #endregion
+    }
+
+    }
+
